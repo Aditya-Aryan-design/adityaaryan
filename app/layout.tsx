b61070cfc3f03dbf80a9b3ff51e22183d7dac4a7
Aditya-Aryan-design <113804917+Aditya-Aryan-design@users.noreply.github.com>
2024-01-09 22:54:24 +0530

    created a simple redux store

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import {Providers} from '@/redux/providers'
 import './globals.css'
+import { Header } from '@/components/Header'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Providers>
+          <Header />
+        {children}
+        </Providers>
+        </body>
     </html>
   )
 }
---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useAppSelector, useAppDispatch } from '@/redux/hooks'
+import { decrement, increment } from '@/redux/slice/theme'
+
+export function Header() {
+  const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch()
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
